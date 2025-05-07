ff397a205b0c917efb5cf15e1ed9af92da06d542
Euxis <46424154+Euxis@users.noreply.github.com>
2025-05-06 20:34:48 -0400

    initial commit

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,114 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Movement parameters")] 
+    [Tooltip("How much force is applied to the player when moving")]
+    [SerializeField] private float moveSpeed;
+    
+    [Tooltip("How much force is applied to the player when jumping")]
+    [SerializeField] private float jumpSpeed;
+    
+    [Tooltip("How much force is applied to the player when dodging")]
+    [SerializeField] private float dodgeSpeed;
+    
+    [Tooltip("Player's maximum movement speed")]
+    [SerializeField] private float maxSpeed;
+
+    [Header("References")] 
+    [SerializeField] private Rigidbody2D rb;
+
+    // Movement variables
+    private Vector2 direction;
+    private int directionInt;
+
+    // Jump variables
+    // Can the player jump?
+    private bool isLanded = false;
+    // Can the player cancel the jump?
+    private bool completedJump = false;
+    // Has the player jumped? (As opposed to just falling)
+    private bool hasJumped = false;
+
+    private void FixedUpdate()
+    {
+        if (directionInt != 0) rb.linearVelocityX = directionInt * moveSpeed;
+        rb.linearVelocityX = Mathf.Clamp(rb.linearVelocityX, -maxSpeed, maxSpeed);
+    }
+
+    private void Update()
+    {
+        CheckLand();
+
+    }
+
+    /// <summary>
+    /// Controls ground movement for player.
+    /// </summary>
+    /// <param name="context"></param>
+    public void GroundMovement(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+
+        if (context.performed)
+        {
+            if (input.x > 0)
+            {
+                directionInt = 1;
+            }
+            else if (input.x < 0)
+            {
+                directionInt = -1;
+            }
+        }
+        else
+        {
+            rb.linearVelocityX = 0;
+            directionInt = 0;
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            if (isLanded)
+            {
+                hasJumped = true;
+                rb.AddForceY(jumpSpeed, ForceMode2D.Impulse); 
+
+                Debug.Log("Jumping");
+                
+                isLanded = false;
+            }
+        }
+
+        if (context.canceled && !completedJump && hasJumped)
+        {
+            rb.linearVelocityY = 0;
+            completedJump = true;
+        }
+    }
+
+    /// <summary>
+    /// Checks below player if they are grounded.
+    /// </summary>
+    private void CheckLand()
+    {
+        if (Physics2D.OverlapBox(rb.position + 0.75f * Vector2.down, new Vector2(0.95f, 0.75f), 0, LayerMask.GetMask("Floor")))
+        {
+            Debug.Log("landed");
+            isLanded = true;
+            completedJump = false;
+            hasJumped = false;
+        }
+        else isLanded = false;
+     
+
+        Debug.DrawLine(rb.position, rb.position + 0.75f * Vector2.down, Color.red);
+    }
+}
